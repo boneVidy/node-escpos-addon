bb38d0cf51b505efa559d8fd208ae7bd7baded9e
boneVidy <120648590@qq.com>
2018-09-11 09:52:08 +0800

    lpt list

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
     const addon = require('./build/Debug/addon.node');
     const iconv = require('iconv-lite');
 
-    const list  = addon.GetUsbDeviceList();
+    const list  = addon.GetUsbDeviceList('com/lpt');
     console.log(list);
-    const printers = list.filter(item => item.service ==='usbprint' || item.deviceName==='USB 打印支持'|| item.service ==='WinUSB');
-    const content = iconv.encode("你好啊\n halo cpp!\\n\n\n\n\nn\n\n\n\n", 'GB18030')
+    // // const printers = list.filter(item => item.service ==='usbprint' || item.deviceName==='USB 打印支持'|| item.service ==='WinUSB');
+    // const content = iconv.encode("你好啊\n halo cpp!\\n\n\n\n\nn\n\n\n\n", 'GB18030')
    
-    addon.PrintRaw(printers[0].path, content);
-    
-
+    // // addon.PrintRaw('LPT1:', content);
+    // const ret = addon.PrintRaw('LPT1', content);
+    // console.log(ret);
+    // console.log(process)
     while(true){}
 
