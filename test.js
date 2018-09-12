a9278e0c6a8e919a1fd527200b10516116182a85
boneVidy <120648590@qq.com>
2018-09-12 16:29:22 +0800

    lpt 设备获取

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
     const addon = require('./build/Debug/addon.node');
     const iconv = require('iconv-lite');
 
-    const list  = addon.GetUsbDeviceList('com/lpt');
-    console.log(list);
+    // const list  = addon.GetUsbDeviceList('USB');
+    // console.log(list);
+
+    console.log('lpt',addon.GetUsbDeviceList('LPT') )
     // // const printers = list.filter(item => item.service ==='usbprint' || item.deviceName==='USB 打印支持'|| item.service ==='WinUSB');
     // const content = iconv.encode("你好啊\n halo cpp!\\n\n\n\n\nn\n\n\n\n", 'GB18030')
    
     // // addon.PrintRaw('LPT1:', content);
     // const ret = addon.PrintRaw('LPT1', content);
-    // console.log(ret);
+    // console.log(ret);Mouse
+
     // console.log(process)
     while(true){}
 
